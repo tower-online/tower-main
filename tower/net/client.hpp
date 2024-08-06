8e40e03c048b3d6d8d42bfeba5bb11be54c684da
Sanito Gonzalez <ddalkisoup23@gmail.com>
2024-08-06 16:35:27 +0900

    Delete client.cpp

--- tower/net/client.hpp
+++ tower/net/client.hpp
@@ -28,4 +28,30 @@ private:
     const std::function<void(std::shared_ptr<Client>&&, std::vector<uint8_t>&&)> _packet_received;
     std::atomic<bool> _is_running {false};
 };
+
+inline Client::Client(boost::asio::io_context& ctx, tcp::socket&& socket, const uint32_t id,
+    std::function<void(std::shared_ptr<Client>&&, std::vector<uint8_t>&&)>&& packet_received)
+    : id(id),
+    _connection(ctx, std::move(socket),
+        [this](std::vector<uint8_t>&& buffer) {
+            _packet_received(shared_from_this(), std::move(buffer));
+        },
+        [this] { disconnected.notify(shared_from_this()); }),
+    _packet_received(std::move(packet_received)) {}
+
+inline void Client::start() {
+    if (_is_running.exchange(true)) return;
+
+    _connection.open();
+}
+
+inline void Client::stop() {
+    if (!_is_running.exchange(false)) return;
+
+    _connection.disconnect();
+}
+
+inline void Client::send_packet(std::shared_ptr<flatbuffers::DetachedBuffer> buffer) {
+    _connection.send_packet(std::move(buffer));
+}
 }
